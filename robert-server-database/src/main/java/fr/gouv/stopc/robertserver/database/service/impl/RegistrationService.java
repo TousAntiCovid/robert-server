23c62bdb9894cab5ec2e81e982b83020efcfa5d3
Deniro StopCovid <10502-deniro-stopcovid@users.noreply.gitlab.inria.fr>
2020-07-16 23:04:16 +0200

    Release/v1.4.1

--- robert-server-database/src/main/java/fr/gouv/stopc/robertserver/database/service/impl/RegistrationService.java
+++ robert-server-database/src/main/java/fr/gouv/stopc/robertserver/database/service/impl/RegistrationService.java
@@ -17,33 +17,30 @@ import org.springframework.util.CollectionUtils;
 @Service
 public class RegistrationService implements IRegistrationService {
 
-    private RegistrationRepository registrationRepository;
+	private RegistrationRepository registrationRepository;
 
-    @Inject
-    public RegistrationService(RegistrationRepository registrationRepository) {
-        this.registrationRepository = registrationRepository;
-    }
+	@Inject
+	public RegistrationService(RegistrationRepository registrationRepository) {
+		this.registrationRepository = registrationRepository;
+	}
 
-    @Override
-    public Optional<Registration> createRegistration(byte[] id) {
-        return Optional.ofNullable(Registration.builder()
-                .permanentIdentifier(id)
-                .build())
-                .map(this.registrationRepository::insert);
-    }
+	@Override
+	public Optional<Registration> createRegistration(byte[] id) {
+		return Optional.ofNullable(Registration.builder().permanentIdentifier(id).build())
+				.map(this.registrationRepository::insert);
+	}
 
-    @Override
-    public Optional<Registration> findById(byte[] id) {
-        return this.registrationRepository.findById(id);
-    }
+	@Override
+	public Optional<Registration> findById(byte[] id) {
+		return this.registrationRepository.findById(id);
+	}
 
-    @Override
-    public Optional<Registration> saveRegistration(Registration registration) {
-        return Optional.ofNullable(registration)
-                .map(this.registrationRepository::save);
-    }
+	@Override
+	public Optional<Registration> saveRegistration(Registration registration) {
+		return Optional.ofNullable(registration).map(this.registrationRepository::save);
+	}
 
-    @Override
+	@Override
 	public void saveAll(final List<Registration> registrations) {
 
 		if (!CollectionUtils.isEmpty(registrations)) {
@@ -52,51 +49,59 @@ public class RegistrationService implements IRegistrationService {
 	}
 
 	@Override
-    public void delete(Registration registration) {
-        Optional.ofNullable(registration).ifPresent(this.registrationRepository::delete);
-    }
+	public void delete(Registration registration) {
+		Optional.ofNullable(registration).ifPresent(this.registrationRepository::delete);
+	}
 
-    @Override
-    public void deleteAll() {
-        this.registrationRepository.deleteAll();
-    }
+	@Override
+	public void deleteAll() {
+		this.registrationRepository.deleteAll();
+	}
 
-    @Override
-    public List<Registration> findAll() {
-        return this.registrationRepository.findAll();
-    }
+	@Override
+	public List<Registration> findAll() {
+		return this.registrationRepository.findAll();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Long countNbUsersAtRiskAgain() {
-        return this.registrationRepository.countNbUsersAtRiskAgain();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Long countNbUsersAtRiskAgain() {
+		return this.registrationRepository.countNbUsersAtRiskAgain();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Long countNbUsersAtRiskAndNotNotified() {
-        return this.registrationRepository.countNbUsersAtRiskAndNotNotified();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Long countNbUsersAtRiskAndNotNotified() {
+		return this.registrationRepository.countNbUsersAtRiskAndNotNotified();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Long countNbUsersNotified() {
-        return this.registrationRepository.countNbUsersNotified();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Long countNbUsersNotified() {
+		return this.registrationRepository.countNbUsersNotified();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Long countNbExposedUsersButNotAtRisk() {
-        return this.registrationRepository.countNbExposedUsersButNotAtRisk();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Long countNbExposedUsersButNotAtRisk() {
+		return this.registrationRepository.countNbExposedUsersButNotAtRisk();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Long countNbNotifiedUsersScoredAgain() {
+		return this.registrationRepository.countNbNotifiedUsersScoredAgain();
+	}
 
 	@Override
 	public Long count() {
@@ -104,7 +109,7 @@ public class RegistrationService implements IRegistrationService {
 	}
 
 	@Override
-	public Long countNbUsersWithOldEpochExpositions(int minEpochId){
+	public Long countNbUsersWithOldEpochExpositions(int minEpochId) {
 		return registrationRepository.countNbUsersWithOldEpochExpositions(minEpochId);
 	}
 }
