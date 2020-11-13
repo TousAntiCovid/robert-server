cabf6015e65c87392530f5074a25e3345f404a7c
Calocedre <calocedre-tac@inria.fr>
2020-11-13 09:47:54 +0100

    formatting

--- tac-warning-ws-rest/src/main/java/fr/gouv/tacw/ws/utils/UriConstants.java
+++ tac-warning-ws-rest/src/main/java/fr/gouv/tacw/ws/utils/UriConstants.java
@@ -1,9 +1,9 @@
 package fr.gouv.tacw.ws.utils;
 
 public interface UriConstants {
-	
+
 	public static final String REPORT = "/report";
-    public static final String STATUS = "/status";
-    
-    public static final String API_V1 = "/v1";
+	public static final String STATUS = "/status";
+
+	public static final String API_V1 = "/v1";
 }
