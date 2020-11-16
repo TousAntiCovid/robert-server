c34694791d741a2cc3da3b83dfc6f6f8424e33f2
Calocedre <calocedre-tac@inria.fr>
2020-11-16 17:29:03 +0100

    organize imports

--- tac-warning/tac-warning-ws-rest/src/test/java/fr/gouv/tacw/ws/TacWarningIntegrationTests.java
+++ tac-warning/tac-warning-ws-rest/src/test/java/fr/gouv/tacw/ws/TacWarningIntegrationTests.java
@@ -1,7 +1,6 @@
 package fr.gouv.tacw.ws;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,9 +21,9 @@ import fr.gouv.tacw.ws.dto.ReportResponseDto;
 import fr.gouv.tacw.ws.utils.UriConstants;
 import fr.gouv.tacw.ws.vo.ExposureStatusRequestVo;
 import fr.gouv.tacw.ws.vo.QRCodeVo;
-import fr.gouv.tacw.ws.vo.VisitVo;
 import fr.gouv.tacw.ws.vo.ReportRequestVo;
 import fr.gouv.tacw.ws.vo.VisitTokenVo;
+import fr.gouv.tacw.ws.vo.VisitVo;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TacWarningIntegrationTests {
