fcaec744c8105442bbb59b1456485930dd4db1cc
Calocedre <calocedre-tac@inria.fr>
2020-11-24 10:58:16 +0100

    enable jwt auth in tests and add integration tests for it

--- tac-warning/tac-warning-ws-rest/src/main/java/fr/gouv/tacw/ws/service/impl/AuthorizationServiceImpl.java
+++ tac-warning/tac-warning-ws-rest/src/main/java/fr/gouv/tacw/ws/service/impl/AuthorizationServiceImpl.java
@@ -10,7 +10,6 @@ import fr.gouv.tacw.ws.exception.TacWarningUnauthorizedException;
 import fr.gouv.tacw.ws.service.AuthorizationService;
 import fr.gouv.tacw.ws.utils.PropertyLoader;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class AuthorizationServiceImpl implements AuthorizationService {
 	private PropertyLoader propertyLoader;
-
+	
 	public AuthorizationServiceImpl(PropertyLoader propertyLoader) {
 		this.propertyLoader = propertyLoader;
 	}
@@ -34,13 +33,11 @@ public class AuthorizationServiceImpl implements AuthorizationService {
 	}
 
 	private void verifyJWT(String token) throws TacWarningUnauthorizedException {
-		SignatureAlgorithm algo = SignatureAlgorithm.RS256; // TODO validate with ANSSI which algo to use, move to
-															// proper place
 		PublicKey jwtPublicKey;
 		// TODO use Vault to store this key
 		try {
 			byte[] encoded = Decoders.BASE64.decode(this.propertyLoader.getJwtPublicKey());
-			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			KeyFactory keyFactory = KeyFactory.getInstance(algo.getFamilyName());
 			X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
 			jwtPublicKey = keyFactory.generatePublic(keySpec);
 			Jwts.parserBuilder().setSigningKey(jwtPublicKey).build().parseClaimsJws(token);
--- tac-warning/tac-warning-ws-rest/src/test/java/fr/gouv/tacw/ws/TacWarningWsRestReportTests.java
+++ tac-warning/tac-warning-ws-rest/src/test/java/fr/gouv/tacw/ws/TacWarningWsRestReportTests.java
@@ -3,7 +3,15 @@ package fr.gouv.tacw.ws;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,12 +28,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
+import fr.gouv.tacw.ws.service.AuthorizationService;
 import fr.gouv.tacw.ws.service.WarningService;
+import fr.gouv.tacw.ws.utils.PropertyLoader;
 import fr.gouv.tacw.ws.utils.UriConstants;
 import fr.gouv.tacw.ws.vo.QRCodeVo;
 import fr.gouv.tacw.ws.vo.ReportRequestVo;
 import fr.gouv.tacw.ws.vo.TokenTypeVo;
 import fr.gouv.tacw.ws.vo.VisitVo;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @MockBean(WarningService.class)
@@ -35,7 +50,10 @@ class TacWarningWsRestReportTests {
 
 	@Autowired
 	private WarningService warningService;
-
+	
+	@Value("${robert.jwt.privatekey}")
+	private String jwtPrivateKey;
+	
 	@Value("${controller.path.prefix}" + UriConstants.API_V1)
 	private String pathPrefixV1;
 
@@ -46,6 +64,28 @@ class TacWarningWsRestReportTests {
 				String.class);
 
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+	@Test
+	void testWhenReportRequestWithMissingAuthenticationThenGetBadRequest() {
+		ReportRequestVo reportRequest = new ReportRequestVo(new ArrayList<VisitVo>());
+		ResponseEntity<String> response = restTemplate.postForEntity(pathPrefixV1 + UriConstants.REPORT, reportRequest,
+				String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+	}
+	
+	@Test
+	void testWhenReportRequestWithNonValidAuthenticationThenGetUnauthorized() {
+		ReportRequestVo reportRequest = new ReportRequestVo(new ArrayList<VisitVo>());
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBearerAuth("invalid JWT token");
+		HttpEntity<ReportRequestVo> reportEntity = new HttpEntity<>(reportRequest, headers);
+		
+		ResponseEntity<String> response = restTemplate.postForEntity(pathPrefixV1 + UriConstants.REPORT, reportEntity,
+				String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
 	}
 
 	@Test
@@ -178,7 +218,7 @@ class TacWarningWsRestReportTests {
 				String.class);
 
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-	}		
+	}
 	
 	@Test
 	void testWhenReportRequestWithValidVisitThenRequestSucceed() throws JSONException {
@@ -196,7 +236,7 @@ class TacWarningWsRestReportTests {
 				+ "   ]\n"
 				+ "}\n"
 				+ "";
-
+		
 		ResponseEntity<String> response = restTemplate.postForEntity(
 				pathPrefixV1 + UriConstants.REPORT, 
 				new HttpEntity<String>(json, this.newJsonHeaderWithBearer()),
@@ -224,6 +264,27 @@ class TacWarningWsRestReportTests {
 	}
 
 	protected void addBearerAuthTo(HttpHeaders headers) {
-		headers.setBearerAuth("sample-bearer");
+		Date now = new Date();
+		Date expiration = new Date(now.getTime() + 60000); 
+		try {
+			String jwtToken = this.newJwtToken(now, expiration);
+			headers.setBearerAuth(jwtToken);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}	
+
+	protected String newJwtToken(Date now, Date expiration) throws NoSuchAlgorithmException, InvalidKeySpecException {
+		PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(Decoders.BASE64.decode(jwtPrivateKey));
+		KeyFactory keyFactory = KeyFactory.getInstance(AuthorizationService.algo.getFamilyName());
+		PrivateKey jwtPrivateKey = keyFactory.generatePrivate(privateKeySpec);
+
+		return Jwts.builder()
+				.setHeaderParam("type", "JWT")
+				.setIssuedAt(now)
+				.setExpiration(expiration)
+				.signWith(jwtPrivateKey, SignatureAlgorithm.RS256)
+				.compact();
+	}
+
 }
