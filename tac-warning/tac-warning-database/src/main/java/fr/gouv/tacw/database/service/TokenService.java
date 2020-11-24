55e090eaec506c131b964fc2252d03ca229a0507
Ravenale Tac <ravenale-tac@inria.fr>
2020-11-24 13:43:13 +0100

    make SECONDS_PER_DAY static

--- tac-warning/tac-warning-database/src/main/java/fr/gouv/tacw/database/service/TokenService.java
+++ tac-warning/tac-warning-database/src/main/java/fr/gouv/tacw/database/service/TokenService.java
@@ -21,6 +21,8 @@ public class TokenService {
 	@Value("${tacw.database.visit_token_retention_period_days}")
 	private long visitTokenRetentionPeriodDays;
 	
+	private static final long SECONDS_PER_DAY = 86400;
+
 	public void registerExposedStaticToken(long timestamp, String token) {
 		staticTokenRepository.save(new ExposedStaticVisitTokenEntity(timestamp, token));
 	}
@@ -35,10 +37,8 @@ public class TokenService {
 	
 	@Scheduled(cron="${tacw.database.visit_token_deletion_job_cron_expression}")
 	public void deleteExpiredTokens() {
-		final long SECONDS_PER_DAY = 86400;
 		final long currentNtpTime = TimeUtils.convertUnixMillistoNtpSeconds(System.currentTimeMillis());
 		final long retentionStart = currentNtpTime - (visitTokenRetentionPeriodDays * SECONDS_PER_DAY);
 		staticTokenRepository.deleteByTimestampLessThan(retentionStart);
-		
 	}
 }
