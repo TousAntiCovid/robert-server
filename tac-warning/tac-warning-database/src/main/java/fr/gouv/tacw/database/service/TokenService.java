ed92e0b0a0f5e69dc3ead26161f6e4e52acf63ad
Ravenale Tac <ravenale-tac@inria.fr>
2020-11-24 10:56:15 +0100

    implement expired tokens deletion

--- tac-warning/tac-warning-database/src/main/java/fr/gouv/tacw/database/service/TokenService.java
+++ tac-warning/tac-warning-database/src/main/java/fr/gouv/tacw/database/service/TokenService.java
@@ -4,7 +4,11 @@ package fr.gouv.tacw.database.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import fr.gouv.stopc.robert.server.common.utils.TimeUtils;
 
 import fr.gouv.tacw.database.model.ExposedStaticVisitTokenEntity;
 import fr.gouv.tacw.database.repository.StaticTokenRepository;
@@ -13,7 +17,10 @@ import fr.gouv.tacw.database.repository.StaticTokenRepository;
 public class TokenService {
 	@Autowired
 	StaticTokenRepository staticTokenRepository;
-
+	
+	@Value("${tacw.database.visit_token_retention_period_days}")
+	private long visitTokenRetentionPeriodDays;
+	
 	public void registerExposedStaticToken(long timestamp, String token) {
 		staticTokenRepository.save(new ExposedStaticVisitTokenEntity(timestamp, token));
 	}
@@ -21,7 +28,17 @@ public class TokenService {
 	public void registerExposedStaticTokens(List<ExposedStaticVisitTokenEntity> exposedStaticVisitTokenEntities ) {
 		staticTokenRepository.saveAll(exposedStaticVisitTokenEntities);
 	}
+	
 	public boolean exposedStaticTokensIncludes(String token) {
 		return staticTokenRepository.findByToken(token).isPresent();
+	}
+	
+	@Scheduled(cron="${tacw.database.visit_token_deletion_job_cron_expression}")
+	public void deleteExpiredTokens() {
+		final long SECONDS_PER_DAY = 86400;
+		final long currentNtpTime = TimeUtils.convertUnixMillistoNtpSeconds(System.currentTimeMillis());
+		final long retentionStart = currentNtpTime - (visitTokenRetentionPeriodDays * SECONDS_PER_DAY);
+		staticTokenRepository.deleteByTimestampLessThan(retentionStart);
+		
 	}
 }
--- tac-warning/tac-warning-database/src/test/java/fr/gouv/tacw/database/TokenServiceTests.java
+++ tac-warning/tac-warning-database/src/test/java/fr/gouv/tacw/database/TokenServiceTests.java
@@ -4,9 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import fr.gouv.stopc.robert.server.common.utils.TimeUtils;
 import fr.gouv.tacw.database.service.TokenService;
 
 @SpringBootTest
@@ -14,6 +16,9 @@ import fr.gouv.tacw.database.service.TokenService;
 class TokenServiceTests {
 	@Autowired
 	TokenService tokenService;
+	
+	@Value("${tacw.database.visit_token_retention_period_days}")
+	private long retentionDays;
 
 	@Test
 	void testRegisteredStaticTokenIsIncludedInInfectedTokens() {
@@ -28,5 +33,43 @@ class TokenServiceTests {
 		String token = "0YWN3LXR5cGUiOiJTVEFUSUMiLCJ0YWN3LXZlcnNpb24iOjEsImVyc";
 
 		assertThat(tokenService.exposedStaticTokensIncludes(token)).isEqualTo(false);
+	}
+	
+	@Test
+	void testDeleteExpiredTokens() {
+		final String[] expired_tokens = {
+				"ac831a7dd6cbe40751ac8d434bfa65cf8ae804133691283bdf2b3b113aea0001",
+				"ac831a7dd6cbe40751ac8d434bfa65cf8ae804133691283bdf2b3b113aea0002",
+				"ac831a7dd6cbe40751ac8d434bfa65cf8ae804133691283bdf2b3b113aea0003",
+				"ac831a7dd6cbe40751ac8d434bfa65cf8ae804133691283bdf2b3b113aea0004",
+				"ac831a7dd6cbe40751ac8d434bfa65cf8ae804133691283bdf2b3b113aea0005",
+		};
+		
+		final String[] valid_tokens = {
+				"ac831a7dd6cbe40751ac8d434bfa65cf8ae804133691283bdf2b3b113aea1001",
+				"ac831a7dd6cbe40751ac8d434bfa65cf8ae804133691283bdf2b3b113aea1002",
+				"ac831a7dd6cbe40751ac8d434bfa65cf8ae804133691283bdf2b3b113aea1003",
+		};
+		
+		final long currentNtpTime = TimeUtils.convertUnixMillistoNtpSeconds(System.currentTimeMillis());
+		final long windowStart = currentNtpTime - (retentionDays*86400);
+		
+		for(String token: expired_tokens) {
+			tokenService.registerExposedStaticToken(windowStart - 150, token);
+		}
+		
+		for(String token: valid_tokens) {
+			tokenService.registerExposedStaticToken(windowStart + 500, token);
+		}
+		
+		tokenService.deleteExpiredTokens();
+		
+		for(String token: expired_tokens) {
+			assertThat(tokenService.exposedStaticTokensIncludes(token)).isEqualTo(false);
+		}
+		
+		for(String token: valid_tokens) {
+	        assertThat(tokenService.exposedStaticTokensIncludes(token)).isEqualTo(true);
+		}
 	}
 }
