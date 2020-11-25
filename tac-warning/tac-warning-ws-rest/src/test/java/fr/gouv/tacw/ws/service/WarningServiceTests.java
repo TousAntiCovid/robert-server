ab6f5c39a126c9a81becd87b6ee65322bdea257a
Calocedre <calocedre-tac@inria.fr>
2020-11-25 23:24:39 +0100

    restore commented assertion

--- tac-warning/tac-warning-ws-rest/src/test/java/fr/gouv/tacw/ws/service/WarningServiceTests.java
+++ tac-warning/tac-warning-ws-rest/src/test/java/fr/gouv/tacw/ws/service/WarningServiceTests.java
@@ -1,29 +1,33 @@
 package fr.gouv.tacw.ws.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import fr.gouv.tacw.database.model.ExposedStaticVisitTokenEntity;
 import fr.gouv.tacw.database.service.TokenService;
 import fr.gouv.tacw.database.service.TokenServiceImpl;
+import fr.gouv.tacw.model.ExposedTokenGenerator;
 import fr.gouv.tacw.ws.vo.ExposureStatusRequestVo;
 import fr.gouv.tacw.ws.vo.QRCodeVo;
 import fr.gouv.tacw.ws.vo.ReportRequestVo;
 import fr.gouv.tacw.ws.vo.TokenTypeVo;
 import fr.gouv.tacw.ws.vo.VenueCategoryVo;
+import fr.gouv.tacw.ws.vo.VenueTypeVo;
 import fr.gouv.tacw.ws.vo.VisitTokenVo;
 import fr.gouv.tacw.ws.vo.VisitVo;
 import fr.gouv.tacw.ws.vo.mapper.TokenMapper;
-import fr.gouv.tacw.ws.vo.TokenTypeVo;
-import fr.gouv.tacw.ws.vo.VenueCategoryVo;
-import fr.gouv.tacw.ws.vo.VenueTypeVo;
 
 
 @SpringBootTest(classes = { WarningServiceImpl.class, TokenServiceImpl.class, TokenMapper.class, ExposureStatusServiceImpl.class })
@@ -35,6 +39,9 @@ public class WarningServiceTests {
 	@Autowired
 	private WarningService warningService;
 
+	@Captor
+	ArgumentCaptor<List<ExposedStaticVisitTokenEntity>> staticTokensCaptor;
+	
 	@Test
 	public void testStatusOfVisitTokenNotInfectedIsNotAtRisk() {
 		List<VisitTokenVo> visitTokens = new ArrayList<VisitTokenVo>();
@@ -63,8 +70,9 @@ public class WarningServiceTests {
 		visits.add(new VisitVo("12345", 
 				new QRCodeVo(TokenTypeVo.STATIC, VenueTypeVo.N, VenueCategoryVo.CAT1, 60, "UUID")));
 
+
 		warningService.reportVisitsWhenInfected(new ReportRequestVo(visits));
-		//TODO  Assertion to update
-		// verify(tokenService, times(ExposedTokenGenerator.numberOfGeneratedTokens())).registerExposedStaticToken(anyLong(), anyString());
+		verify(tokenService).registerExposedStaticTokens(staticTokensCaptor.capture());
+		assertThat(staticTokensCaptor.getValue().size()).isEqualTo(ExposedTokenGenerator.numberOfGeneratedTokens());
 	}
 }
