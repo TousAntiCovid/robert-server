7b49be8061535f8f507c99ba6349ed93fb2c4c82
x-etac <eucalyptus-tac@inria.fr>
2020-11-26 17:41:59 +0100

    Creation of the system-test project.

---
+++ system-test/README.md
@@ -0,0 +1,5 @@
+mvn compile
+
+Edit main to create tests
+
+Objective being to create End to End scenarios for testing 
---
+++ system-test/src/main/java/fr/gouv/tac/systemtest/IntegrationAppCLI.java
@@ -0,0 +1,31 @@
+package fr.gouv.tac.systemtest;
+// Import classes:
+import org.openapitools.client.ApiClient;
+import org.openapitools.client.ApiException;
+import org.openapitools.client.Configuration;
+import org.openapitools.client.api.DefaultApi;
+import org.openapitools.client.model.*;
+
+public class IntegrationAppCLI {
+    public static void main(String[] args) {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setBasePath("http://localhost/api/tac-warning/v1");
+
+        DefaultApi apiInstance = new DefaultApi(defaultClient);
+        ExposureStatusRequest exposureStatusRequest = new ExposureStatusRequest(); // ExposureStatusRequest |
+        VisitToken visitToken = new VisitToken();
+        visitToken.setType(VisitToken.TypeEnum.STATIC);
+        visitToken.setPayload("7625d7d4a5b45f98f95ef25ed8951da566609a60");
+        exposureStatusRequest.addVisitTokensItem(visitToken);
+        try {
+            ExposureStatusResponse result = apiInstance.eSR(exposureStatusRequest);
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling DefaultApi#eSR");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
+    }
+}
