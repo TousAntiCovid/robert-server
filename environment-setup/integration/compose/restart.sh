e7401bba56cdb8b8ebeab536e78bb55eb22ae8b0
Dragonnier TAC <dragonnier-tac@inria.fr>
2021-01-12 18:27:09 +0100

    fix stopAll.sh

--- environment-setup/integration/compose/restart.sh
+++ environment-setup/integration/compose/restart.sh
@@ -11,13 +11,13 @@ docker-compose -f docker-compose-robert-server.yaml --env-file .env.local pull
 
 #Down 
 
-docker-compose -f docker-compose-orange-captcha.yml -v --env-file .env.local down
-docker-compose -f docker-compose-proxy.yaml -v --env-file .env.local down
-docker-compose -f docker-compose-robert-crypto.yml -v --env-file .env.local down
-docker-compose -f docker-compose-robert-push.yml -v --env-file .env.local down
-docker-compose -f docker-compose-robert-submission.yml -v --env-file .env.local down
-docker-compose -f docker-compose-tacw.yml -v --env-file .env.local down
-docker-compose -f docker-compose-robert-server.yaml -v --env-file .env.local down
+docker-compose -f docker-compose-orange-captcha.yml --env-file .env.local down
+docker-compose -f docker-compose-proxy.yaml --env-file .env.local down
+docker-compose -f docker-compose-robert-crypto.yml --env-file .env.local down
+docker-compose -f docker-compose-robert-push.yml --env-file .env.local down
+docker-compose -f docker-compose-robert-submission.yml --env-file .env.local down
+docker-compose -f docker-compose-tacw.yml --env-file .env.local down
+docker-compose -f docker-compose-robert-server.yaml --env-file .env.local down
 
 #start
 docker-compose -f docker-compose-orange-captcha.yml  --env-file .env.local up -d
--- environment-setup/integration/compose/stopAll.sh
+++ environment-setup/integration/compose/stopAll.sh
@@ -3,12 +3,12 @@
 
 #Down 
 
-docker-compose -f docker-compose-orange-captcha.yml -v down 
-docker-compose -f docker-compose-proxy.yaml -v down
-docker-compose -f docker-compose-robert-crypto.yml -v down
-docker-compose -f docker-compose-robert-push.yml -v down
-docker-compose -f docker-compose-robert-submission.yml -v down
-docker-compose -f docker-compose-tacw.yml -v down
-docker-compose -f docker-compose-robert-server.yaml -v down
+docker-compose -f docker-compose-orange-captcha.yml down 
+docker-compose -f docker-compose-proxy.yaml down
+docker-compose -f docker-compose-robert-crypto.yml down
+docker-compose -f docker-compose-robert-push.yml down
+docker-compose -f docker-compose-robert-submission.yml down
+docker-compose -f docker-compose-tacw.yml down
+docker-compose -f docker-compose-robert-server.yaml down
 
 
