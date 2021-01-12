36d740fbf3f140e1b0c9cdd0fe933895b86a319e
Calocedre <calocedre-tac@inria.fr>
2021-01-12 10:51:06 +0100

    Adding /v5 on deleteExposureHistory

--- robert-server/robert-server-ws-rest/src/main/java/fr/gouv/stopc/robertserver/ws/controller/IDeleteHistoryController.java
+++ robert-server/robert-server-ws-rest/src/main/java/fr/gouv/stopc/robertserver/ws/controller/IDeleteHistoryController.java
@@ -18,7 +18,8 @@ import fr.gouv.stopc.robertserver.ws.vo.DeleteHistoryRequestVo;
 
 @RestController
 @RequestMapping(value = {"${controller.path.prefix}" + UriConstants.API_V2,
-        "${controller.path.prefix}" + UriConstants.API_V3, "${controller.path.prefix}" + UriConstants.API_V4})
+        "${controller.path.prefix}" + UriConstants.API_V3, "${controller.path.prefix}" + UriConstants.API_V4,
+        "${controller.path.prefix}" + UriConstants.API_V5 })
 @Consumes(MediaType.APPLICATION_JSON_VALUE)
 @Produces(MediaType.APPLICATION_JSON_VALUE)
 public interface IDeleteHistoryController {
