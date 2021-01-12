eaa0392e5a7d34334ae33ef1cde2a9320501d692
Calocedre <calocedre-tac@inria.fr>
2021-01-12 10:54:22 +0100

    Adding /v5 on /register

--- robert-server/robert-server-ws-rest/src/main/java/fr/gouv/stopc/robertserver/ws/controller/IRegisterController.java
+++ robert-server/robert-server-ws-rest/src/main/java/fr/gouv/stopc/robertserver/ws/controller/IRegisterController.java
@@ -16,9 +16,9 @@ import fr.gouv.stopc.robertserver.ws.dto.RegisterResponseDto;
 import fr.gouv.stopc.robertserver.ws.exception.RobertServerException;
 import fr.gouv.stopc.robertserver.ws.utils.UriConstants;
 
-@RequestMapping(value = {"${controller.path.prefix}" + UriConstants.API_V2, 
-		"${controller.path.prefix}" + UriConstants.API_V3,
-		"${controller.path.prefix}" + UriConstants.API_V4})
+@RestController
+@RequestMapping(value = {"${controller.path.prefix}" + UriConstants.API_V2, "${controller.path.prefix}" + UriConstants.API_V3,
+        "${controller.path.prefix}" + UriConstants.API_V4, "${controller.path.prefix}" + UriConstants.API_V5 })
 @Consumes(MediaType.APPLICATION_JSON_VALUE)
 @Produces(MediaType.APPLICATION_JSON_VALUE)
 public interface IRegisterController {
