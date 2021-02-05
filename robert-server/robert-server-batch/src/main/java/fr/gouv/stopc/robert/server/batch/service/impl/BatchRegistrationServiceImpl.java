f6585c755b1888a0de47c59c8bc7ba7e3531c54d
Calocedre <calocedre-tac@inria.fr>
2021-02-05 17:12:24 +0100

    Comment why notified is not reset

--- robert-server/robert-server-batch/src/main/java/fr/gouv/stopc/robert/server/batch/service/impl/BatchRegistrationServiceImpl.java
+++ robert-server/robert-server-batch/src/main/java/fr/gouv/stopc/robert/server/batch/service/impl/BatchRegistrationServiceImpl.java
@@ -71,7 +71,8 @@ public class BatchRegistrationServiceImpl {
             registration.setLatestRiskEpoch(newLatestRiskEpoch);
             log.info("Updating latest risk epoch {}", newLatestRiskEpoch);
             registration.setAtRisk(true);
-            registration.setNotified(false);
+            // Do not reset isNotified since it is used to compute the number of notifications
+            // It is up to the client to know if it should notify (new risk) or not given the risk change or not.
             isRegistrationAtRisk = true;
         }
 
