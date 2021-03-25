1c60659d3afda7a64dc2adb11dfdb5cec3a0675b
x-AOrang <12847-x-AOrang@users.noreply.gitlab.inria.fr>
2021-03-25 10:51:54 +0100

    Fixed code reviews

--- clea/clea-ws-rest/src/main/java/fr/gouv/clea/ws/configuration/SecurityConfiguration.java
+++ clea/clea-ws-rest/src/main/java/fr/gouv/clea/ws/configuration/SecurityConfiguration.java
@@ -3,13 +3,11 @@ package fr.gouv.clea.ws.configuration;
 import fr.inria.clea.lsp.LocationSpecificPartDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.annotation.RequestScope;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    @RequestScope
     public LocationSpecificPartDecoder getLocationSpecificPartDecoder() {
         return new LocationSpecificPartDecoder(null);
     }
