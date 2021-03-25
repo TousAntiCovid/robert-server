ab5e806204bb1cb0affc78133d50554e4febe11c
Calocedre <calocedre-tac@inria.fr>
2021-03-25 15:37:01 +0100

    add VisitExpositionAggregatorService

--- clea/clea-venue-consumer/src/main/java/fr/gouv/clea/consumer/service/impl/ConsumerService.java
+++ clea/clea-venue-consumer/src/main/java/fr/gouv/clea/consumer/service/impl/ConsumerService.java
@@ -1,48 +1,41 @@
 package fr.gouv.clea.consumer.service.impl;
 
-import fr.gouv.clea.consumer.model.DecodedVisit;
-import fr.gouv.clea.consumer.model.ExposedVisitEntity;
-import fr.gouv.clea.consumer.model.Visit;
-import fr.gouv.clea.consumer.service.IAggregateService;
-import fr.gouv.clea.consumer.service.IConsumerService;
-import fr.gouv.clea.consumer.service.IDecodedVisitService;
-import fr.gouv.clea.consumer.service.IExposedVisitEntityService;
-import fr.gouv.clea.consumer.utils.MessageFormatter;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Optional;
+import fr.gouv.clea.consumer.model.DecodedVisit;
+import fr.gouv.clea.consumer.model.Visit;
+import fr.gouv.clea.consumer.service.IVisitExpositionAggregatorService;
+import fr.gouv.clea.consumer.service.IConsumerService;
+import fr.gouv.clea.consumer.service.IDecodedVisitService;
+import fr.gouv.clea.consumer.utils.MessageFormatter;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class ConsumerService implements IConsumerService {
 
     private final IDecodedVisitService decodedVisitService;
-    private final IAggregateService aggregateService;
-    private final IExposedVisitEntityService exposedVisitEntityService;
+    private final IVisitExpositionAggregatorService visitExpositionAggregatorService;
 
     @Autowired
     public ConsumerService(
             IDecodedVisitService decodedVisitService,
-            IAggregateService aggregateService,
-            IExposedVisitEntityService exposedVisitEntityService
-    ) {
+            IVisitExpositionAggregatorService visitExpositionAggregatorService) {
         this.decodedVisitService = decodedVisitService;
-        this.aggregateService = aggregateService;
-        this.exposedVisitEntityService = exposedVisitEntityService;
+        this.visitExpositionAggregatorService = visitExpositionAggregatorService;
     }
 
     @Override
     @KafkaListener(topics = "${spring.kafka.template.default-topic}")
     public void consume(DecodedVisit decodedVisit) {
         log.info("[locationTemporaryPublicId: {}, qrCodeScanTime: {}] retrieved from queue", MessageFormatter.truncateUUID(decodedVisit.getStringLocationTemporaryPublicId()), decodedVisit.getQrCodeScanTime());
-        Optional<Visit> optional = decodedVisitService.decryptAndValidate(decodedVisit);
-        if (optional.isPresent()) {
-            List<ExposedVisitEntity> exposedVisitEntities = aggregateService.aggregate(optional.get());
-            exposedVisitEntityService.persistMany(exposedVisitEntities);
+        Optional<Visit> optionalVisit = decodedVisitService.decryptAndValidate(decodedVisit);
+        if (optionalVisit.isPresent()) {
+            visitExpositionAggregatorService.updateExposureCount(optionalVisit.get());
         }
     }
 }
