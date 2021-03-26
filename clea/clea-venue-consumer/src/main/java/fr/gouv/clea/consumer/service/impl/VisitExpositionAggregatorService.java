6d5d942615d98ee0519c22313fde4aba9c5f90c4
x-AOrang <12847-x-AOrang@users.noreply.gitlab.inria.fr>
2021-03-26 15:53:31 +0100

    Fixed code reviews

--- clea/clea-venue-consumer/src/main/java/fr/gouv/clea/consumer/service/impl/VisitExpositionAggregatorService.java
+++ clea/clea-venue-consumer/src/main/java/fr/gouv/clea/consumer/service/impl/VisitExpositionAggregatorService.java
@@ -48,8 +48,8 @@ public class VisitExpositionAggregatorService implements IVisitExpositionAggrega
                                 .filter(exposedVisit -> exposedVisit.getTimeSlot() == slotIndex)
                                 .findFirst()
                                 .ifPresentOrElse(
-                                        exposedVisit -> toUpdate.add(this.updateExposureVisit(visit, exposedVisit)),
-                                        () -> toPersist.add(this.newExposureVisit(visit, slotIndex))
+                                        exposedVisit -> toUpdate.add(this.updateExposedVisit(visit, exposedVisit)),
+                                        () -> toPersist.add(this.newExposedVisit(visit, slotIndex))
                                 )
                 );
 
@@ -65,7 +65,7 @@ public class VisitExpositionAggregatorService implements IVisitExpositionAggrega
         return TimeUtils.instantFromTimestamp((long) visit.getCompressedPeriodStartTime() * TimeUtils.NB_SECONDS_PER_HOUR);
     }
 
-    protected ExposedVisitEntity updateExposureVisit(Visit visit, ExposedVisitEntity exposedVisit) {
+    protected ExposedVisitEntity updateExposedVisit(Visit visit, ExposedVisitEntity exposedVisit) {
         if (visit.isBackward()) {
             exposedVisit.setBackwardVisits(exposedVisit.getBackwardVisits() + 1);
         } else {
@@ -74,7 +74,7 @@ public class VisitExpositionAggregatorService implements IVisitExpositionAggrega
         return exposedVisit;
     }
 
-    protected ExposedVisitEntity newExposureVisit(Visit visit, int slotIndex) {
+    protected ExposedVisitEntity newExposedVisit(Visit visit, int slotIndex) {
         // TODO: visit.getPeriodStart returning an Instant
         long periodStart = this.periodStartInstant(visit).toEpochMilli();
         return ExposedVisitEntity.builder()
