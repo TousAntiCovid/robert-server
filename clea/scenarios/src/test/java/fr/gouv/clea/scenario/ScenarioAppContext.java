c302c8bdc61002fedf342a932fe9d8e08df0b70c
Calocedre <calocedre-tac@inria.fr>
2021-03-26 18:32:23 +0100

    generate keys

--- clea/scenarios/src/test/java/fr/gouv/clea/scenario/ScenarioAppContext.java
+++ clea/scenarios/src/test/java/fr/gouv/clea/scenario/ScenarioAppContext.java
@@ -2,35 +2,48 @@ package fr.gouv.clea.scenario;
 
 import fr.gouv.clea.client.service.CleaClient;
 import fr.gouv.tacw.qr.LocationQrCodeGenerator;
+import fr.inria.clea.lsp.CleaEciesEncoder;
 import fr.inria.clea.lsp.CleaEncryptionException;
 import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.util.encoders.Hex;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Slf4j
 public class ScenarioAppContext {
-    private final String manualContactTracingAuthorityPublicKey;
-    private final String serverAuthorityPublicKey;
-    private final String permanentLocationSecretKey;
     private final Map<String, Integer> venueCategories;
     private final Map<String, Integer> venueTypes;
     private Map<String, CleaClient> visitors;
     private Map<String, LocationQrCodeGenerator> locations;
+    private String manualContactTracingAuthorityPublicKey;
+    private String serverAuthorityPublicKey;
 
-    public ScenarioAppContext() {
+    public ScenarioAppContext() throws Exception {
         visitors = new HashMap<String, CleaClient>(10);
         locations = new HashMap<String, LocationQrCodeGenerator>(10);
-        manualContactTracingAuthorityPublicKey = "TBD";
-        serverAuthorityPublicKey = "TBD";
-        permanentLocationSecretKey = "TBD";
+        this.generateKeys();
         venueTypes = new HashMap<String, Integer> (
                 Map.of("restaurant", 1));
         venueCategories = new HashMap<String, Integer> (
                 Map.of("NUMBER_1", 1));
+    }
 
+    public void generateKeys() throws Exception {
+        CleaEciesEncoder cleaEciesEncoder = new CleaEciesEncoder();
+        String[] serverAuthorityKeyPair = cleaEciesEncoder.genKeysPair(true);
+        this.serverAuthorityPublicKey = serverAuthorityKeyPair[1];
+        System.out.println("Server Authority Private Key: " + serverAuthorityKeyPair[0]);
+        System.out.println("Server Authority Public Key : " + this.serverAuthorityPublicKey);
+        
+        String[] manualContactTracingAuthorityKeyPair = cleaEciesEncoder.genKeysPair(true);
+        this.manualContactTracingAuthorityPublicKey = serverAuthorityKeyPair[1];
+        System.out.println("Server Authority Private Key: " + serverAuthorityKeyPair[0]);
+        System.out.println("Server Authority Public Key : " + this.manualContactTracingAuthorityPublicKey);
     }
 
     public CleaClient getOrCreateVisitor(String name) {
@@ -44,9 +57,11 @@ public class ScenarioAppContext {
 
     private LocationQrCodeGenerator createLocation(String locationName, Instant periodStartTime,
     String venueType, String venueCategory1, Integer venueCapacity, Duration qrCodeRenewalInterval) throws CleaEncryptionException{
+        log.info("Creating location " + locationName);
         long qrCodeRenewalIntervalLong = qrCodeRenewalInterval.getSeconds();
         int qrCodeRenewalIntervalExponentCompact = (int)(Math.log(qrCodeRenewalIntervalLong)/Math.log(2));
 
+        final String permanentLocationSecretKey = Hex.toHexString(UUID.randomUUID().toString().getBytes());
         LocationQrCodeGenerator location = LocationQrCodeGenerator.builder()
                                                                 .countryCode(250) // France QR Code
                                                                 .staff(false) // not used in test declaration as of now
@@ -65,9 +80,9 @@ public class ScenarioAppContext {
 
     public LocationQrCodeGenerator getOrCreateLocation(String locationName, Instant periodStartTime,
             String venueType, String venueCategory1, Integer venueCapacity, Duration qrCodeRenewalInterval) throws CleaEncryptionException {
-        return (locations.containsKey(locationName) ?
+        return locations.containsKey(locationName) ?
                 locations.get(locationName) :
-                this.createLocation(locationName, periodStartTime, venueType, venueCategory1, venueCapacity, qrCodeRenewalInterval));
+                this.createLocation(locationName, periodStartTime, venueType, venueCategory1, venueCapacity, qrCodeRenewalInterval);
     }
 
     public LocationQrCodeGenerator getLocation(String locationName){
