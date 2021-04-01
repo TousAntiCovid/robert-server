d7da07fc144b1ebe0dd62a437299ac5c89f9d39a
Framboise Orange <12541-x-FrOrang@users.noreply.gitlab.inria.fr>
2021-04-01 14:45:09 +0200

    use the default spring producer factory in order to easily get its configuration to set it on the custom producer factory

--- analytics-server/src/main/java/fr/gouv/tac/analytics/server/config/kafka/KafkaConfiguration.java
+++ analytics-server/src/main/java/fr/gouv/tac/analytics/server/config/kafka/KafkaConfiguration.java
@@ -1,9 +1,5 @@
 package fr.gouv.tac.analytics.server.config.kafka;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import fr.gouv.tac.analytics.server.model.kafka.Analytics;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,22 +8,25 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
-import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.gouv.tac.analytics.server.model.kafka.Analytics;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 @Configuration
+@Slf4j
 public class KafkaConfiguration {
-
-    @Value(value = "${spring.kafka.bootstrap-servers}")
-    private String bootstrapAddress;
 
     @Value(value = "${spring.kafka.template.default-topic}")
     private String defaultTopic;
 
     @Bean
-    public KafkaTemplate<String, Analytics> analyticsKafkaTemplate(final ObjectMapper objectMapper) {
-        final Map<String, Object> props = Map.of(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+    public KafkaTemplate<String, Analytics> analyticsKafkaTemplate(final ObjectMapper objectMapper, final ProducerFactory defaultSpringProducerFactory) {
 
-        final ProducerFactory<String, Analytics> producerFactory = new DefaultKafkaProducerFactory<>(props,
+        log.debug("conf default producer factory : {}", defaultSpringProducerFactory.getConfigurationProperties().toString());
+
+        //a custom producer factory is instantiated in order to take advantage of the spring object mapper on date time management
+        final ProducerFactory<String, Analytics> producerFactory = new DefaultKafkaProducerFactory<>(defaultSpringProducerFactory.getConfigurationProperties(),
                 new StringSerializer(),
                 new JsonSerializer<Analytics>(objectMapper));
 
