76d651be53ef30919516599873815d889517f277
x-AOrang <12847-x-AOrang@users.noreply.gitlab.inria.fr>
2021-04-09 12:38:41 +0200

    Minor

--- clea/clea-ws-rest/src/test/java/fr/gouv/clea/ws/controller/CleaControllerAuthEnabledTest.java
+++ clea/clea-ws-rest/src/test/java/fr/gouv/clea/ws/controller/CleaControllerAuthEnabledTest.java
@@ -1,5 +1,8 @@
 package fr.gouv.clea.ws.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.gouv.clea.ws.dto.ApiError;
 import fr.gouv.clea.ws.utils.UriConstants;
 import fr.gouv.clea.ws.vo.ReportRequest;
 import fr.gouv.clea.ws.vo.Visit;
@@ -17,6 +20,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 
+import java.time.Instant;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,8 +35,11 @@ class CleaControllerAuthEnabledTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    void testWhenReportRequestWithMissingAuthenticationThenGetBadRequest() {
+    void testWhenReportRequestWithMissingAuthenticationThenUNAUTHORIZED() throws JsonProcessingException {
         List<Visit> visits = List.of(
                 new Visit(RandomStringUtils.randomAlphanumeric(20), RandomUtils.nextLong()),
                 new Visit(RandomStringUtils.randomAlphanumeric(20), RandomUtils.nextLong()),
@@ -42,10 +49,16 @@ class CleaControllerAuthEnabledTest {
         ResponseEntity<String> response = restTemplate.postForEntity(pathPrefix + UriConstants.REPORT, request, String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+
+        ApiError apiError = objectMapper.readValue(response.getBody(), ApiError.class);
+        assertThat(apiError.getHttpStatus()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+        assertThat(apiError.getTimestamp()).isBefore(Instant.now());
+        assertThat(apiError.getMessage()).isEqualTo("Could not be authorized (Missing authorisation header/token)");
+        assertThat(apiError.getValidationErrors()).isEmpty();
     }
 
     @Test
-    void testWhenReportRequestWithNonValidAuthenticationThenGetUnauthorized() {
+    void testWhenReportRequestWithNonValidAuthenticationThenGetFORBIDDEN() throws JsonProcessingException {
         List<Visit> visits = List.of(
                 new Visit(RandomStringUtils.randomAlphanumeric(20), RandomUtils.nextLong()),
                 new Visit(RandomStringUtils.randomAlphanumeric(20), RandomUtils.nextLong()),
@@ -58,5 +71,11 @@ class CleaControllerAuthEnabledTest {
         ResponseEntity<String> response = restTemplate.postForEntity(pathPrefix + UriConstants.REPORT, reportEntity, String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+
+        ApiError apiError = objectMapper.readValue(response.getBody(), ApiError.class);
+        assertThat(apiError.getHttpStatus()).isEqualTo(HttpStatus.FORBIDDEN.value());
+        assertThat(apiError.getTimestamp()).isBefore(Instant.now());
+        assertThat(apiError.getMessage()).isEqualTo("Could not be authenticated (Authorisation header/token invalid)");
+        assertThat(apiError.getValidationErrors()).isEmpty();
     }
 }
