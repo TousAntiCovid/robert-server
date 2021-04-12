d7f50ca737bca1bd2521d564ee0eeaf0517552c3
avocatier <avocatier-tac@inria.fr>
2021-04-12 09:04:00 +0200

    clea-client: remove unused import

--- clea/clea-client/src/main/java/fr/gouv/clea/client/utils/UrlContentReader.java
+++ clea/clea-client/src/main/java/fr/gouv/clea/client/utils/UrlContentReader.java
@@ -5,7 +5,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.file.Path;
 import java.util.Optional;
 
 import lombok.extern.slf4j.Slf4j;
