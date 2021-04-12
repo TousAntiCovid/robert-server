98d06f8dea9d0ed0a5883ac451df885ce0e7e86f
Combava Orange <-13340-x-CoOrang@users.noreply.gitlab.inria.fr>
2021-04-12 17:37:45 +0200

    [clea-batch]: drop Field/Column ExposedVisit.qrCodeScanTime

--- clea/clea-batch/src/main/resources/db/migration/postgresql/V3__init_intermediary_db.sql
+++ clea/clea-batch/src/main/resources/db/migration/postgresql/V3__init_intermediary_db.sql
@@ -2,6 +2,11 @@
 -- as superadmin user:
 -- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+
+-- Needs: Clea-venue-Consumer
+ALTER TABLE exposed_visits DROP COLUMN IF EXISTS qr_code_scan_time;
+
+-- Needs: Clea-Batch
 CREATE TABLE cluster_periods
 (
     id                          VARCHAR(64) NOT NULL DEFAULT uuid_generate_v4(),
@@ -18,5 +23,20 @@ CREATE TABLE cluster_periods
 
     PRIMARY KEY (id)
 );
-
 CREATE INDEX IF NOT EXISTS cluster_periods_ltid ON cluster_periods (ltid);
+
+-- Needs: Clea-Statistiques
+CREATE TABLE stat_location
+(
+    period           TIMESTAMP(0) WITH TIME ZONE NOT NULL,
+    venue_type       INT NOT NULL,
+    venue_category1  INT NOT NULL,
+    venue_category2  INT NOT NULL,
+    backward_visits  BIGINT NOT NULL,
+    forward_visits   BIGINT NOT NULL
+);
+CREATE INDEX IF NOT EXISTS statloc_period ON stat_location(period);
+CREATE INDEX IF NOT EXISTS statloc_venue  ON stat_location(venue_type, venue_category1, venue_category2);
+
+
+ 
