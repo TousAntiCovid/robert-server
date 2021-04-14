8aaf5840b573b2ffd8ddffd7e9ec9fb1876ea5a6
Calocedre <calocedre-tac@inria.fr>
2021-04-14 22:13:28 +0200

    use yaml to define scoring properties

--- clea/clea-scoring-conf/src/test/java/fr/gouv/clea/clea/scoring/configuration/ExposureTimeConfigurationTest.java
+++ clea/clea-scoring-conf/src/test/java/fr/gouv/clea/clea/scoring/configuration/ExposureTimeConfigurationTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @EnableConfigurationProperties(value = ExposureTimeConfiguration.class)
 @ContextConfiguration(classes = { ExposureTimeConfigurationConverter.class })
-@TestPropertySource("classpath:application.properties")
+@SpringBootTest
 public class ExposureTimeConfigurationTest {
     @Autowired
     private ExposureTimeConfiguration configuration;
