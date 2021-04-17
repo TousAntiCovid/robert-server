a27a6d82e320793f47f512a78ed4f736384f11d3
Calocedre <calocedre-tac@inria.fr>
2021-04-18 00:43:28 +0200

    validation of configuration

--- clea/clea-venue-consumer/src/main/java/fr/gouv/clea/consumer/configuration/VenueConsumerConfiguration.java
+++ clea/clea-venue-consumer/src/main/java/fr/gouv/clea/consumer/configuration/VenueConsumerConfiguration.java
@@ -1,18 +1,29 @@
 package fr.gouv.clea.consumer.configuration;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Validated
 @Configuration
 @ConfigurationProperties(prefix = "clea.conf")
 public class VenueConsumerConfiguration {
+    @Min(value = 600)
     private long durationUnitInSeconds;
+    @Positive
     private int driftBetweenDeviceAndOfficialTimeInSecs;
+    @Positive
     private int cleaClockDriftInSecs;
+    @Min(value = 10)
+    @Max(value = 30)
     private int retentionDurationInDays;
 }
