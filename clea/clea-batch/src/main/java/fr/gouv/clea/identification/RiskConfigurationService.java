e3bf1a2c46c99be62d8c327c8a9dd2da69a71006
Calocedre <calocedre-tac@inria.fr>
2021-04-21 10:42:33 +0200

    remove unused files

--- clea/clea-batch/src/main/java/fr/gouv/clea/identification/RiskConfigurationService.java
+++ clea/clea-batch/src/main/java/fr/gouv/clea/identification/RiskConfigurationService.java
@@ -18,8 +18,6 @@ public class RiskConfigurationService {
 	@Autowired
 	private RiskConfiguration riskConfiguration;
 
-	static final RiskLevelConfig DEFAULT = new RiskLevelConfig(3, 1, 3.0f, 2.0f);
-
 	public Optional<RiskRule> evaluate(int venueType, int venueCategory1, int venueCategory2) {
 		return Optional.of(riskConfiguration.getConfigurationFor(venueType, venueCategory1, venueCategory2));
 	}
