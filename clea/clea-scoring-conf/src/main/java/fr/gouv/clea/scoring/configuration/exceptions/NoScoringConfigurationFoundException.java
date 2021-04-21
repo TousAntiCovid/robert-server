3b7883d0e33caec82bbc7475ef6d8098a7ae2c5b
Calocedre <calocedre-tac@inria.fr>
2021-04-21 15:18:01 +0200

    add serial version id

--- clea/clea-scoring-conf/src/main/java/fr/gouv/clea/scoring/configuration/exceptions/NoScoringConfigurationFoundException.java
+++ clea/clea-scoring-conf/src/main/java/fr/gouv/clea/scoring/configuration/exceptions/NoScoringConfigurationFoundException.java
@@ -1,4 +1,4 @@
-package fr.gouv.clea.clea.scoring.configuration.exceptions;
+package fr.gouv.clea.scoring.configuration.exceptions;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class NoScoringConfigurationFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
 
     public NoScoringConfigurationFoundException(String message) {
         super(message);
