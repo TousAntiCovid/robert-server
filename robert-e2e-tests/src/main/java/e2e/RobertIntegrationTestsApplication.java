e52459d9e92af1c6ec23e8beb90aa07a52705938
Jujube Orange <13631-x-JOrang@users.noreply.gitlab.inria.fr>
2021-10-26 08:21:23 +0000

    feat(ci): rename docker-compose.yaml ➡ .yml so
    this way ci template can trigger integration tests

--- robert-e2e-tests/src/main/java/e2e/RobertIntegrationTestsApplication.java
+++ robert-e2e-tests/src/main/java/e2e/RobertIntegrationTestsApplication.java
@@ -1,16 +1,15 @@
 package e2e;
 
-import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
-
-import java.util.List;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+import java.util.List;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -33,7 +32,7 @@ public class RobertIntegrationTestsApplication {
         System.out.println("getTestsSucceededCount() - " + summary.getTestsSucceededCount());
         failures.forEach(failure -> System.out.println("failure - " + failure.getException()));
 
-        var exitCode = failures.size()>0 ? 1 : 0;
+        var exitCode = failures.size() > 0 ? 1 : 0;
         System.exit(exitCode);
-      }
+    }
 }
