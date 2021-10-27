94b34da9e50c720fa93a13816a454f9331838528
Jamalac Orange <14492-x-JaOrang@users.noreply.gitlab.inria.fr>
2021-10-27 15:42:26 +0000

    refactor(app): change submission url to https

--- robert-server-ws-rest/src/main/java/fr/gouv/stopc/robertserver/ws/service/impl/RestApiServiceImpl.java
+++ robert-server-ws-rest/src/main/java/fr/gouv/stopc/robertserver/ws/service/impl/RestApiServiceImpl.java
@@ -1,13 +1,11 @@
 package fr.gouv.stopc.robertserver.ws.service.impl;
 
-import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-
-import javax.inject.Inject;
-
+import fr.gouv.stopc.robertserver.ws.dto.VerifyResponseDto;
+import fr.gouv.stopc.robertserver.ws.service.IRestApiService;
+import fr.gouv.stopc.robertserver.ws.utils.PropertyLoader;
+import fr.gouv.stopc.robertserver.ws.vo.PushInfoVo;
+import io.micrometer.core.instrument.util.StringUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,14 +13,15 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import fr.gouv.stopc.robertserver.ws.dto.VerifyResponseDto;
-import fr.gouv.stopc.robertserver.ws.service.IRestApiService;
-import fr.gouv.stopc.robertserver.ws.utils.PropertyLoader;
-import fr.gouv.stopc.robertserver.ws.vo.PushInfoVo;
-import io.micrometer.core.instrument.util.StringUtils;
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
+
+import javax.inject.Inject;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -35,22 +34,25 @@ public class RestApiServiceImpl implements IRestApiService {
     private final WebClient webClient;
 
     @Inject
-    public RestApiServiceImpl(final PropertyLoader propertyLoader, final RestTemplate restTemplate, final WebClient webClient) {
+    public RestApiServiceImpl(final PropertyLoader propertyLoader, final RestTemplate restTemplate,
+            final WebClient webClient) {
         this.propertyLoader = propertyLoader;
         this.restTemplate = restTemplate;
         this.webClient = webClient;
     }
 
     @Override
-    public Optional<VerifyResponseDto> verifyReportToken(String token)  {
+    public Optional<VerifyResponseDto> verifyReportToken(String token) {
 
-        if(StringUtils.isEmpty(token)) {
+        if (StringUtils.isEmpty(token)) {
             return Optional.empty();
         }
 
         try {
-            ResponseEntity<VerifyResponseDto> response = restTemplate.getForEntity(buildReportTokenVerificationURI(token),
-                    VerifyResponseDto.class);
+            ResponseEntity<VerifyResponseDto> response = restTemplate.getForEntity(
+                    buildReportTokenVerificationURI(token),
+                    VerifyResponseDto.class
+            );
 
             return Optional.ofNullable(response.getBody());
         } catch (RestClientException e) {
@@ -60,15 +62,11 @@ public class RestApiServiceImpl implements IRestApiService {
         return Optional.empty();
     }
 
-    private URI buildReportTokenVerificationURI(String token) {
-
-        return UriComponentsBuilder.newInstance().scheme("http")
-                .host(this.propertyLoader.getServerCodeHost())
-                .port(this.propertyLoader.getServerCodePort())
-                .path(this.propertyLoader.getServerCodeVerificationPath())
-                .queryParam("code", token)
+    private URI buildReportTokenVerificationURI(String code) {
+        return UriComponentsBuilder.fromUri(this.propertyLoader.getServerCodeUrl())
+                .path("/api/v1/verify")
+                .queryParam("code", code)
                 .build()
-                .encode()
                 .toUri();
     }
 
@@ -109,15 +107,15 @@ public class RestApiServiceImpl implements IRestApiService {
                     .bodyToMono(Void.class);
 
             response.doOnSuccess(t -> log.info("Register to push notification successful"))
-            .doOnError(error ->  log.error("Register to push notification failed due to {}", error.getMessage()))
-            .subscribe();
+                    .doOnError(error -> log.error("Register to push notification failed due to {}", error.getMessage()))
+                    .subscribe();
         }
     }
 
     @Override
     public void unregisterPushNotif(String pushToken) {
 
-        if(StringUtils.isNotBlank(pushToken)) {
+        if (StringUtils.isNotBlank(pushToken)) {
             Mono<Void> response = this.webClient
                     .delete()
                     .uri(this.buildUnregistertPushNotifURI(pushToken))
@@ -125,29 +123,30 @@ public class RestApiServiceImpl implements IRestApiService {
                     .bodyToMono(Void.class);
 
             response.doOnSuccess(t -> log.info("Unregister to push notification successful"))
-            .doOnError(error ->  log.error("Unregister to push notification failed due to {}", error.getMessage()))
-            .subscribe();
+                    .doOnError(
+                            error -> log.error("Unregister to push notification failed due to {}", error.getMessage())
+                    )
+                    .subscribe();
         }
     }
 
     private boolean isValidPushInfo(final PushInfoVo pushInfoVo) {
 
-
-        if(Objects.isNull(pushInfoVo)) {
+        if (Objects.isNull(pushInfoVo)) {
             return false;
         }
 
-        if(StringUtils.isBlank(pushInfoVo.getToken())) {
+        if (StringUtils.isBlank(pushInfoVo.getToken())) {
             log.warn("Token is mandatory to register to push notification");
             return false;
         }
 
-        if(StringUtils.isBlank(pushInfoVo.getTimezone())) {
+        if (StringUtils.isBlank(pushInfoVo.getTimezone())) {
             log.warn("Timezone is mandatory to register to push notification");
             return false;
         }
 
-        if(StringUtils.isBlank(pushInfoVo.getLocale())) {
+        if (StringUtils.isBlank(pushInfoVo.getLocale())) {
             log.warn("Locale is mandatory to register to push notification");
             return false;
         }
