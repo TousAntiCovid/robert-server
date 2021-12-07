8f9966741874110abcfbbd81c56f7fde70512618
Jamalac Orange <14492-x-JaOrang@users.noreply.gitlab.inria.fr>
2021-12-07 18:04:50 +0100

    feature(test): minor changes

--- robert-e2e-tests/src/main/java/fr/gouv/stopc/e2e/external/database/mongodb/model/EpochExposition.java
+++ robert-e2e-tests/src/main/java/fr/gouv/stopc/e2e/external/database/mongodb/model/EpochExposition.java
@@ -1,9 +1,7 @@
 package fr.gouv.stopc.e2e.external.database.mongodb.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -11,8 +9,6 @@ import java.util.List;
  * An epoch exposition reflects the exposure of the user (temporal and frequency
  * information).
  */
-@NoArgsConstructor
-@AllArgsConstructor
 @Data
 @Builder
 public class EpochExposition {
