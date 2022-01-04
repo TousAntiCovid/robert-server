58dbb855bf7d8d1e27e67e9a985f645a9cccb597
Jujube Orange <13631-x-JOrang@users.noreply.gitlab.inria.fr>
2022-01-04 17:53:24 +0100

    feat(monitoring): publish a metric with app version

---
+++ robert-server-batch/src/main/java/fr/gouv/stopc/robert/server/batch/PublishVersionOnStartupListener.java
@@ -0,0 +1,31 @@
+package fr.gouv.stopc.robert.server.batch;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.boot.info.GitProperties;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PublishVersionOnStartupListener {
+
+    private final MeterRegistry meterRegistry;
+
+    private final BuildProperties buildProperties;
+
+    private final GitProperties gitProperties;
+
+    @EventListener
+    public void publishVersion(ApplicationStartedEvent event) {
+        Counter.builder("app")
+                .description("Informations about application")
+                .tag("version", buildProperties.getVersion())
+                .tag("commit", gitProperties.getShortCommitId())
+                .register(meterRegistry)
+                .increment();
+    }
+}
