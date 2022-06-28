d59b55eea70841afa91b27a4f2610396eacded97
Jujube Orange <13631-x-JOrang@users.noreply.gitlab.inria.fr>
2022-06-28 09:01:57 +0000

    test(metrics): review MetricsManager

--- robert-server-batch/src/test/java/fr/gouv/stopc/robert/server/batch/manager/MetricsManager.java
+++ robert-server-batch/src/test/java/fr/gouv/stopc/robert/server/batch/manager/MetricsManager.java
@@ -1,49 +1,46 @@
 package fr.gouv.stopc.robert.server.batch.manager;
 
 import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Timer;
 import org.assertj.core.api.AbstractDoubleAssert;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestExecutionListener;
 
-import java.util.HashMap;
+import java.util.Map;
 
+import static java.util.stream.Collectors.toMap;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class MetricsManager implements TestExecutionListener {
 
     private static MeterRegistry registry;
 
-    private static HashMap<String, Double> metrics = new HashMap<>();
-
-    private void loadAllMetrics() {
-        registry.getMeters().forEach(
-                meter -> {
-                    final var name = meter.getId().getName();
-                    if (meter instanceof Counter) {
-                        metrics.put(name, ((Counter) meter).count());
-                    } else if (meter instanceof Timer) {
-                        metrics.put(name, Double.valueOf(((Timer) meter).count()));
-                    } else if (meter instanceof DistributionSummary) {
-                        metrics.put(name, Double.valueOf(((DistributionSummary) meter).count()));
-                    }
-                }
-        );
-    }
+    private static Map<Meter.Id, Double> metrics;
 
     @Override
     public void beforeTestMethod(TestContext testContext) throws Exception {
         TestExecutionListener.super.beforeTestMethod(testContext);
         registry = testContext.getApplicationContext().getBean(MeterRegistry.class);
-        loadAllMetrics();
+
+        metrics = registry.getMeters()
+                .stream()
+                .collect(toMap(Meter::getId, this::meterValue));
     }
 
-    public static AbstractDoubleAssert<?> assertThatCounterMetricIncrement(String metricLabel) {
-        var newMetricValue = registry.find(metricLabel).counter().count();
-        var increment = newMetricValue - metrics.get(metricLabel);
-        return assertThat(increment).as("Increment between before test method and now");
+    private Double meterValue(Meter meter) {
+        if (meter instanceof Counter) {
+            return ((Counter) meter).count();
+        } else {
+            return -1.0;
+        }
+    }
+
+    public static AbstractDoubleAssert<?> assertThatCounterMetricIncrement(String label, String... tags) {
+        final var counter = registry.counter(label, tags);
+        final var increment = counter.count() - metrics.getOrDefault(counter.getId(), 0.0);
+        return assertThat(increment)
+                .as("Counter value increment for %s %s ", label, String.join(",", tags));
     }
 
 }
