17db3f4815956e357f5ccc59534ecbc7fd63e5a1
Figue Orange <12540-x-FOrang@users.noreply.gitlab.inria.fr>
2022-07-26 14:26:29 +0200

    refacto(api) : changed kpi model

--- robert-server-database/src/main/java/fr/gouv/stopc/robertserver/database/repository/WebserviceKpiCustomRepositoryImpl.java
+++ robert-server-database/src/main/java/fr/gouv/stopc/robertserver/database/repository/WebserviceKpiCustomRepositoryImpl.java
@@ -1,36 +1,38 @@
 package fr.gouv.stopc.robertserver.database.repository;
 
-import fr.gouv.stopc.robertserver.database.model.WebserviceStatistics;
+import fr.gouv.stopc.robertserver.database.model.WebserviceKpi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
 @RequiredArgsConstructor
-public class WebserviceStatisticsCustomRepositoryImpl implements WebserviceStatisticsCustomRepository {
+public class WebserviceKpiCustomRepositoryImpl implements WebserviceKpiCustomRepository {
 
     private final MongoOperations mongoOperations;
 
     @Override
     public void incrementNotifiedUsers() {
-        final var query = new Query();
-        final var update = new Update().inc("notifiedUsers", 1);
+        final var query = new Query().addCriteria(Criteria.where("name").is("notifiedUsers"));
+        final var update = new Update().inc("value", 1);
         final var options = FindAndModifyOptions.options()
                 .upsert(true)
                 .returnNew(true);
 
-        mongoOperations.findAndModify(query, update, options, WebserviceStatistics.class);
+        mongoOperations.findAndModify(query, update, options, WebserviceKpi.class);
     }
 
     @Override
     public void incrementReportsCount() {
-        final var query = new Query();
-        final var update = new Update().inc("reportsCount", 1);
+        final var query = new Query().addCriteria(Criteria.where("name").is("reportsCount"));
+        final var update = new Update().inc("value", 1);
         final var options = FindAndModifyOptions.options()
                 .upsert(true)
                 .returnNew(true);
-        mongoOperations.findAndModify(query, update, options, WebserviceStatistics.class);
+
+        mongoOperations.findAndModify(query, update, options, WebserviceKpi.class);
     }
 
 }
