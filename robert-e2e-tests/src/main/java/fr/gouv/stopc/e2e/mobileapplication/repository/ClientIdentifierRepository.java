0fe95dc1afcef688f8a16e6d42e6705d32e217de
Jujube Orange <13631-x-JOrang@users.noreply.gitlab.inria.fr>
2022-12-01 10:25:52 +0000

    refactor(e2e): remove jpa and replace with spring-data-jdbc

--- robert-e2e-tests/src/main/java/fr/gouv/stopc/e2e/mobileapplication/repository/ClientIdentifierRepository.java
+++ robert-e2e-tests/src/main/java/fr/gouv/stopc/e2e/mobileapplication/repository/ClientIdentifierRepository.java
@@ -1,13 +1,13 @@
 package fr.gouv.stopc.e2e.mobileapplication.repository;
 
 import fr.gouv.stopc.e2e.mobileapplication.repository.model.ClientIdentifier;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ClientIdentifierRepository extends JpaRepository<ClientIdentifier, Long> {
+public interface ClientIdentifierRepository extends CrudRepository<ClientIdentifier, Long> {
 
     Optional<ClientIdentifier> findTopByOrderByIdDesc();
 }
--- robert-e2e-tests/src/main/java/fr/gouv/stopc/e2e/mobileapplication/repository/model/ClientIdentifier.java
+++ robert-e2e-tests/src/main/java/fr/gouv/stopc/e2e/mobileapplication/repository/model/ClientIdentifier.java
@@ -1,41 +1,33 @@
 package fr.gouv.stopc.e2e.mobileapplication.repository.model;
 
-import lombok.*;
-
-import javax.persistence.*;
+import lombok.Builder;
+import lombok.Value;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.Date;
 
-@Entity
-@Table(name = "IDENTITY", indexes = { @Index(name = "IDX_IDA", columnList = "idA") })
-@Data
+@Value
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@Table("identity")
 public class ClientIdentifier {
 
     @Id
-    @ToString.Exclude
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    Long id;
 
-    @ToString.Exclude
-    @Column(name = "idA", unique = true, nullable = false)
-    private String idA;
+    @Column("ida")
+    String idA;
 
-    @ToString.Exclude
-    @Column(name = "key_for_mac", updatable = false, nullable = false)
-    private String keyForMac;
+    @Column("key_for_mac")
+    String keyForMac;
 
-    @ToString.Exclude
-    @Column(name = "key_for_tuples", updatable = false, nullable = false)
-    private String keyForTuples;
+    @Column("key_for_tuples")
+    String keyForTuples;
 
-    @Column(name = "creation_time", updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    protected Date dateCreation;
+    @Column("creation_time")
+    Date dateCreation;
 
-    @Column(name = "last_update")
-    @Temporal(TemporalType.TIMESTAMP)
-    protected Date dateMaj;
+    @Column("last_update")
+    Date dateMaj;
 }
